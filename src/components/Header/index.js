60f97fd661aec9e2867eb7efd9b913499e7e3dc3
Jan Andrade <cloud.andrade16@gmail.com>
2023-05-21 22:11:29 -0300

    few adjusts

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,30 +1,34 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import { Container } from "./styles";
-
-import logoImg from '../../assets/Logo.png';
-
 import { FaSearch, FaShoppingBag } from 'react-icons/fa';
 import { RiMenu3Fill } from 'react-icons/ri';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+
+//import logoImg from '../../assets/Logo.png';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { Container } from './styles';
 
 export function Header({ setMenuIsVisible }) {
-  return (
-    <Container>
-      <section>
-        <img src={logoImg} alt="Logo" />
-        <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/produtos#">Produtos</NavLink>
-          <NavLink to="/sobre">Sobre nós</NavLink>
-          <NavLink to="/contato">Contato</NavLink>
-        </nav>
-      </section>
+	let logoImg = 'CLOUD';
+	return (
+		<Container>
+			<section>
+				{/*
+      <img src={logoImg} alt="Logo" />
+      */}
+				<h1>{logoImg}</h1>
 
-      <section>
-        <FaSearch />
-        <FaShoppingBag />
-        <RiMenu3Fill onClick={() => setMenuIsVisible(true)} className="mobile" />
-      </section>
-    </Container>
-  )
+				<nav>
+					<NavLink to="/">Home</NavLink>
+					<NavLink to="/produtos#">Produtos</NavLink>
+					<NavLink to="/sobre">Sobre nós</NavLink>
+					<NavLink to="/contato">Contato</NavLink>
+				</nav>
+			</section>
+
+			<section>
+				<FaSearch />
+				<FaShoppingBag />
+				<RiMenu3Fill onClick={() => setMenuIsVisible(true)} className="mobile" />
+			</section>
+		</Container>
+	);
 }
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,25 +1,22 @@
-import { useState } from "react";
-import { Header } from "../../components/Header";
-import { HeroBanner } from "../../components/HeroBanner";
-import { MenuMobile } from "../../components/MenuMobile";
-import { OtherModels } from "../../components/OtherModels";
-import { HomeContainer } from "../../styles/pages/HomeStyles";
+import { useState } from 'react';
+
+import { Header } from '../../components/Header';
+import { MenuMobile } from '../../components/MenuMobile';
+import { HomeContainer } from '../../styles/pages/HomeStyles';
 
 export function Cart() {
-  const [menuIsVisible, setMenuIsVisible] = useState(false);
+	const [menuIsVisible, setMenuIsVisible] = useState(false);
 
-  return (
-    <>
-      <MenuMobile
-        menuIsVisible={menuIsVisible}
-        setMenuIsVisible={setMenuIsVisible}
-      />
-      <HomeContainer>
-        <Header setMenuIsVisible={setMenuIsVisible} />
-        <h1>
-          Cart
-        </h1>
-      </HomeContainer>
-    </>
-  )
+	return (
+		<>
+			<MenuMobile
+				menuIsVisible={menuIsVisible}
+				setMenuIsVisible={setMenuIsVisible}
+			/>
+			<HomeContainer>
+				<Header setMenuIsVisible={setMenuIsVisible} />
+				<h1>Cart</h1>
+			</HomeContainer>
+		</>
+	);
 }
